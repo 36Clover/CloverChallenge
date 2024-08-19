d7a81e09d315420d7a20b3a918b2122e57a28dc4
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2024-08-20 03:37:46 +0900

    파일 받기

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 public class App {
     public static void main(String[] args) {
-        ClovaSpeechClient cv = new ClovaSpeechClient();
-        System.out.println(cv.pa_json());
+//        ClovaSpeechClient cv = new ClovaSpeechClient();
+//        System.out.println(cv.pa_json());
     }
 }
