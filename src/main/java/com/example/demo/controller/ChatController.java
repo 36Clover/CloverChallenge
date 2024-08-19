afd02836a2772cd628cb5447f604ad650c3caf3a
GTU9 <sangik9999@naver.com>
2024-08-20 01:02:33 +0900

    .

--- src/main/java/com/example/demo/controller/ChatController.java
+++ src/main/java/com/example/demo/controller/ChatController.java
@@ -17,7 +17,7 @@ public class ChatController {
 
     @PostMapping("/chat")
     public String chat(@RequestParam String prompt) {
-        return openAIService.getChatGPTResponse(prompt);
+        return openAIService.getChatGPTResponse(prompt+ "위 요약본을 토대로 문제를 단답형 10 문제와 답은 단어 하나로만 이루어져 있다. question과 answer으로 이루어진 JSON으로 만들어줘");
     }
 }
 
